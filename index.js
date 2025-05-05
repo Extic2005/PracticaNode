2dc49f9f5d91844ee9030aacc84ef97a546cf3e4
Extic2005 <floresfinipee@gmail.com>
2025-05-05 17:31:41 -0500

    ProyectoNode

---
+++ index.js
@@ -0,0 +1,16 @@
+// ya creado mi index.js
+
+const http = require('http');
+
+// Creo mi servidor
+const server = http.createServer((req, res) => {
+  // Establesco mi encabezado
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  // Envio la respuesta
+  res.end('¡Si lees esto ten un buen dia!');
+});
+////
+server.listen(3000, () => {
+  console.log('Servidor escuchando en http://localhost:3000');
+});
